1c523a07fc74fdd1011a3bdae776357c6aa839a9
Sarah Gill <gills@uchicago.edu>
2021-03-03 15:37:33 -0800

    Update Test metpy.py
    never mind, I was wrong about windchill -using 2m wind, equation assumes 10m wind!

--- build/not in use/Test metpy.py
+++ build/not in use/Test metpy.py
@@ -10,13 +10,21 @@ os.chdir('/Users/Sarah/Documents/GitHub/US-schoolday-temperatures/Data')
 df = pd.read_csv('20-12-28.csv')
 index = 15000
 
-temp = [df['temperature_(F)'][index]]*units.degF
-wind = [df['wind_speed_(mph)'][index]]*units.mph
+df['windchill'] = ''
+for index in range(len(df)):
+    temp = [df['temperature_(F)'][index]]*units.degF
+    wind = [df['wind_speed_(mph)'][index]]*units.mph
+
+
+    df['windchill'][index] = metpy.calc.windchill(temp, wind,
+                        face_level_winds = False,
+                        mask_undefined = True)
+
+
 
 print('windchil', df['with_windchill_(F)'][index],
         'temp', df['temperature_(F)'][index],
         'wind', df['wind_speed_(mph)'][index])
-metpy.calc.windchill(temp, wind,
-                    face_level_winds = True,
-                    mask_undefined = True)
+
+
 

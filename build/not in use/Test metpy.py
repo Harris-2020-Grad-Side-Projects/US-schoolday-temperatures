2bf9c820659dd342c1f572fd87c31acda6357891
Sarah Gill <gills@uchicago.edu>
2021-03-03 15:18:27 -0800

    Update Test metpy.py
    it kind of works, but output is not same as NOAA calculator! why?

--- build/not in use/Test metpy.py
+++ build/not in use/Test metpy.py
@@ -1,9 +1,22 @@
 #Test metpy
-import metpy 
+import metpy.calc 
+from metpy.units import units
 import os
 import pandas as pd
 
 
 os.chdir('/Users/Sarah/Documents/GitHub/US-schoolday-temperatures/Data')
 
-df = pd.read_csv(filename)
+df = pd.read_csv('20-12-28.csv')
+index = 15000
+
+temp = [df['temperature_(F)'][index]]*units.degF
+wind = [df['wind_speed_(mph)'][index]]*units.mph
+
+print('windchil', df['with_windchill_(F)'][index],
+        'temp', df['temperature_(F)'][index],
+        'wind', df['wind_speed_(mph)'][index])
+metpy.calc.windchill(temp, wind,
+                    face_level_winds = True,
+                    mask_undefined = True)
+
